c93be52a523a73a66792902904d06d16c6b06dee
NinjaTyper <35751000+NinjaTyper@users.noreply.github.com>
2024-10-08 20:10:55 -0700

    add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import {useState} from 'react';
-import MessagesContainer from './components/MessagesContainer/MessagesContainer.js';
-import AiModels from './components/AiModels/AiModels.js';
+import MessagesContainer from './components/MessagesContainer/MessagesContainer.js'
+import Counter from './features/counter/counter.js'
 
 function App() {
   return (
     <div className="App">
       <MessagesContainer/>
-      
+      <Counter/>
     </div>
   );
 }
